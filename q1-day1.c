ba1aec0a934aab95f1b4496993f47c86ef5644fc
Pratibha Rawat <Pratibha.35255@stu.upes.ac.in>
2026-09-06 17:20:10 +0530

    Q1 day1 program

---
+++ q1-day1.c
@@ -0,0 +1,10 @@
+// Write a program to input two numbers and display their sum.
+#include <stdio.h>
+int main()
+{
+    int a,b;
+    printf("Enter two numbers: ");
+    scanf("%d %d",&a,&b);
+    printf("Sum = %d",a+b);
+    return 0;
+}
